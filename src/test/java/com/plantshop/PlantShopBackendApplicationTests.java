31bab44e342e21192c2b26fb0e14f945f342d726
mehp2 <hpmegha299@gmail.com>
2022-12-06 12:50:21 +0530

    first commit

---
+++ src/test/java/com/plantshop/PlantShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+@ContextConfiguration
+class PlantShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
